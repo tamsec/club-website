b3cebd9003f9e89dacce18a4d0c96883ce45d01c
saenzjonathan11 <34073257+saenzjonathan11@users.noreply.github.com>
2020-05-20 01:54:00 -0500

    changed admin colors

--- tamucyber/settings.py
+++ tamucyber/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'tamucyber.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': config.db,
+        'NAME': config.db_name,
+        'USER': config.db_user,
+        'PASSWORD': config.db_password,
+        'HOST': config.db_host 
     }
 }
 
